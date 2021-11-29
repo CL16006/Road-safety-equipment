bbec6b48a0f58f983e6b612e38804bb7cc4c5e7e
CL16006 <calderonlopez678@gmail.com>
2021-11-29 12:18:59 -0600

    agregarndo boton de instalar

--- js/app.js
+++ js/app.js
@@ -88,4 +88,21 @@ map.addControl(
     });
   }
 
-  
+  //crear configuraciones para instalar app
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
+});
+
+const installApp = document.getElementById('installBtn');
+
+installApp.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
+});
